276d96e2a09574d69f2af41e670699a6451e910f
vipulkarke <82372312+vipulkarke@users.noreply.github.com>
2021-07-16 22:21:19 +0530

    Update router.js

--- login_system/router.js
+++ login_system/router.js
@@ -7,11 +7,6 @@ const fs = require("fs");
 var uuid = require('uuid')
 var alert = require("alert");
 
-//tupple for login
-// const credential={
-//     email:"admin@gmail.com",
-//     password:"admin"
-// }
 
 
 //logincheck
@@ -21,7 +16,7 @@ connection.connect(function(err) {
     var password = req.body.password;
     console.log(email,password);
     var sql = 'SELECT email,password FROM users where email = ? and password = ?';
-    connection.query(sql, [email, password], function(error, results, fields) {
+    connection.query(sql, [email, password], function(error, results,) {
         if (results.length > 0) {
             req.session.user = email;
             res.redirect('/route/dashboard');
@@ -32,17 +27,6 @@ connection.connect(function(err) {
     });
   });
 });
-
-// user login using tupple 
-// router.post('/login',(req,res)=>{
-//     if(req.body.email == credential.email && req.body.password == credential.password){
-//         req.session.user = req.body.email;
-//        res.redirect('/route/dashboard');
-//     }else{
-//         res.end('invalid username')
-        
-//     }
-// });
 
 // route for dashboard
 router.get('/dashboard',(req,res)=>{
@@ -70,7 +54,6 @@ router.post('/register',(req,res)=>{
 var storage = multer.diskStorage({
     destination: function(req, file,callback){
         var dir = "./upload";
-
         if (!fs.existsSync(dir)){
             fs.mkdirSync(dir);
         }
@@ -80,8 +63,9 @@ var storage = multer.diskStorage({
         callback(null,file.originalname);
     }
 });
-
 var upload = multer({storage:storage}).array('files',20);
+
+
 router.post('/upload',(req,res,next)=>{
     upload(req,res,function(err){
     if(err){
@@ -94,32 +78,20 @@ router.post('/upload',(req,res,next)=>{
 
 
 
-
-// router.get('/showfiles',(req,res)=>{
-//     res.render('showfiles')
-// });
-
+//Route for showfiles
 router.post('/show',(req,res,next)=>{
-    // res.render('showfiles',{user:req.session.user});
+    res.render('showfiles',{user:req.session.user});
+    console.log(user.session.user);
 let directory_name = "./upload";
-
-// Function to get current filenames
-// in directory
 let filenames = fs.readdirSync(directory_name);
-
 console.log("\nFilenames in directory:");
 filenames.forEach((file) => {
     req.file
     res.write('<html><head></head><body><table><tr>  ' +file);
     res.write('</tr>        </table>    </body>    </html>');           
-    console.log("File:", file);
-    console.log("File:", file);
+    console.log("File:", file);   
 });
 });
-
-//new route for upload files and images
-
-
 
 
 module.exports=router;
